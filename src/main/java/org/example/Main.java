90cf01a499609bce1fc549364fc255849fe622b7
Dtan713 <s.donaldtan@gmail.com>
2024-08-08 15:26:10 -0400

    some more of the glab3053

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+}
+
+System.out.println( " ====== Display list of all books ====");
+try {
+        for (Books cc : bookDao.getAllBooks()) {
+int ISBN = cc.getIsbn();
+String BookName = cc.getBookName();
+       System.out.println("======================");
+       System.out.println("ISBN Number :" + ISBN + "and Book name: " + BookName);
+
+   }
+           } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+} catch (SQLException e) {
+        e.printStackTrace();
 }
